f90e561d1b8d1800aa7f3dfd7cb58d28b19048bc
NIKHIL KANOJIA <nikhilkanojia693@gmail.com>
2025-03-16 15:14:24 +0530

    frontend structure created

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <main>
+      <div className="left">
+        <div className="code"></div>
+        <div className="reviewBtn">Review</div>
+      </div>
+      <div className="right"></div>
+    </main>
+    </>
+  )
+}
+
+export default App
